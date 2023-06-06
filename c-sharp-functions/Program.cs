e9cb3a44af37b40bd60d9ac63c2ebdfc2ff110b8
omranali2111 <aljabri1234@hotmail.com>
2023-06-05 23:49:42 -0700

    functions

---
+++ c-sharp-functions/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        bioData("omran", "aljabri", 27);
+    }
+
+    static void bioData(string name,string lastname,int age )
+    {
+        Console.WriteLine("name is" + name);
+        Console.WriteLine("last name is "+  lastname);
+        Console.WriteLine("age is " + age);
+
+    }
+}
