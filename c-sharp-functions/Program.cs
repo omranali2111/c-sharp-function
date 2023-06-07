adb2d8ccf52b20efaf2f6a20e942374150785a93
omranali2111 <aljabri1234@hotmail.com>
2023-06-07 10:20:52 +0400

    recursive function

--- c-sharp-functions/Program.cs
+++ c-sharp-functions/Program.cs
@@ -1,14 +1,29 @@
 ﻿internal class Program
 {
+    
     private static void Main(string[] args)
     {
+        int fact, num;
         bioData("omran", "aljabri", 27);
+
+        
+        Console.Write("Enter a number: ");
+
+        
+        num = Convert.ToInt32(Console.ReadLine());
+
+         Program obj = new Program();
+
+       
+       fact=obj.factorial(num);
+
+        Console.WriteLine("Factorial of "+ num +" is "+ fact);
 
         //switch
         int day;
 
         Console.WriteLine("enter number of a day: ");
-         day= int.Parse(Console.ReadLine());
+        day = int.Parse(Console.ReadLine());
         switch (day)
         {
             case 1:
@@ -40,17 +55,26 @@
     //unsafe
     //static A static local function can't capture local variables or instance state.
     //extern An external local function must be static.
-    static void bioData(string name,string lastname,int age )
+    static void bioData(string name, string lastname, int age)
     {
         Console.WriteLine("name is" + name);
-        Console.WriteLine("last name is "+  lastname);
+        Console.WriteLine("last name is " + lastname);
         Console.WriteLine("age is " + age);
 
     }
 
-  
 
-                 
+    // recursive function 
+    public int factorial(int num)
+    {
+        // termination condition
+        if (num == 0)
+            return 1;
+        else
+            // recursive call
+            return num * factorial(num - 1);
 
 
+
+    }
 }
